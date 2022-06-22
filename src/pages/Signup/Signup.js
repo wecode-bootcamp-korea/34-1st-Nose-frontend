818a5ae1d6734edf2dc92ba9c37c8e19dc1e4a94
Vanessa <bohyunparkv@gmail.com>
2022-06-22 20:49:11 +0900

    ADD: 회원가입 페이지 - UI & 회원가입 버튼 기능

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Signup.scss';
-
-// import { useState, useNavigate } from 'react-router-dom';
 
 const Signup = () => {
   const [userInfo, setUserInfo] = useState({
@@ -10,34 +9,35 @@ const Signup = () => {
     email: '',
     phone_number: '',
     password: '',
-    event_push: '',
   });
   const [accountWarning, setAccountWarning] = useState('');
-  // const [emailWarning, setEmailWarning] = useState('');
-  // const [phoneNumberWarning, setPhoneNumberWarning] = useState('');
-  // const [passwordWarning, setPasswordWarning] = useState('');
+  const [emailWarning, setEmailWarning] = useState('');
+  const [phoneNumberWarning, setPhoneNumberWarning] = useState('');
+  const [passwordWarning, setPasswordWarning] = useState('');
+  const navigate = useNavigate();
 
   const checkAccount = () =>
     !isAccountValid
-      ? setAccountWarning(
-          '아이디는 영문소문자 또는 숫자 4~16자로 입력해 주세요.'
-        )
+      ? setAccountWarning('영문소문자 또는 숫자 4~16자로 입력해 주세요.')
       : setAccountWarning('');
 
   const checkEmail = () =>
     !isEmailValid
-      ? setEmailWarning(
-          '이미 사용중인 이메일입니다. 다른 이메일로 다시 시도해 주세요.'
-        )
+      ? setEmailWarning('@와 .을 포함 시켜주세요.')
       : setEmailWarning('');
 
   const checkPhoneNumber = () =>
     !isPhoneNumberValid
-      ? setPhoneNumber('휴대전화 번호를 입력해주세요.')
-      : setPhoneNumber('');
+      ? setPhoneNumberWarning("'-'를 제외한 숫자만 입력해주세요.")
+      : setPhoneNumberWarning('');
+
+  const checkPassword = () =>
+    !isPasswordValid
+      ? setPasswordWarning('비밀번호는 4~16자리로 이뤄져야 합니다.')
+      : setPasswordWarning('');
 
   // destructuring
-  const { name, account, email, phone_number, password, event_push } = userInfo;
+  const { name, account, email, phone_number, password } = userInfo;
 
   const saveUserInfo = e => {
     const { name, value } = e.target;
@@ -54,33 +54,42 @@ const Signup = () => {
   const isPhoneNumberValid = phoneNumberRegExp.test(phone_number);
   const isPasswordValid = passwordRegExp.test(password);
 
-  const isAllValid =
-    isAccountValid && isEmailValid && isPhoneNumberValid && isPasswordValid;
+  const redWarningAccount = !isAccountValid
+    ? 'redMessageActive'
+    : 'redMessageInactive';
+
+  const redWarningEmail = !isEmailValid
+    ? 'redMessageActive'
+    : 'redMessageInactive';
+
+  const redWarningPhoneNumber = !isPhoneNumberValid
+    ? 'redMessageActive'
+    : 'redMessageInactive';
+
+  const redWarningPassword = !isPasswordValid
+    ? 'redMessageActive'
+    : 'redMessageInactive';
 
   const postUserData = () => {
-    if (isAllValid) {
-      // navigate('/main');
-      alert('success!');
-    } else if (!isAccountValid) {
-      alert('아이디는 영문소문자 또는 숫자 4~16자로 입력해 주세요.');
-    } else if (!isEmailValid) {
-      alert('이미 사용중인 이메일입니다. 다른 이메일로 다시 시도해 주세요.');
-    } else if (!isPhoneNumberValid) {
-      alert('휴대전화 번호를 입력해주세요.');
-    } else if (!isPasswordValid) {
-      alert('비밀번호는 4~16자리로 이뤄져야 합니다.');
-    } else {
-    }
-
-    // 다 통과하지 못하는 경우 & 한 개만 통과하는 경우 & 두 개만 통과하는 경우
-
-    fetch('API 주소', {
+    fetch('http://10.58.7.241:8000/users/signup', {
       method: 'POST',
-      body: JSON.stringify({}),
+      body: JSON.stringify({
+        name: name,
+        account: account,
+        email: email,
+        phone_number: phone_number,
+        password: password,
+      }),
     })
       .then(res => res.json())
-      .then(result => result());
+      .then(result => {
+        if (result.message === 'SUCCESS') {
+          navigate('/login');
+        }
+      });
   };
+  const isAllTrue =
+    isAccountValid && isEmailValid && isPhoneNumberValid && isPasswordValid;
 
   return (
     <div className="loginBox">
@@ -91,7 +100,7 @@ const Signup = () => {
           <input type="text" placeholder="이름을 입력해주세요" name="name" />
           <div className="message" />
         </div>
-        // 밑에꺼 내일 다시 시작
+
         <div className="accountWrapper wrapper">
           <div className="account text">아이디</div>
           <input
@@ -101,8 +110,9 @@ const Signup = () => {
             onChange={saveUserInfo}
             onKeyUp={checkAccount}
           />
-          <span className="warning">{accountWarning}</span>
-          <div className="message" />
+          <div className="message">
+            <span className={redWarningAccount}>{accountWarning}</span>
+          </div>
         </div>
         <div className="emailWrapper wrapper">
           <div className="email text">이메일</div>
@@ -113,7 +123,9 @@ const Signup = () => {
             onChange={saveUserInfo}
             onKeyUp={checkEmail}
           />
-          <div className="message" />
+          <div className="message">
+            <span className={redWarningEmail}>{emailWarning}</span>
+          </div>
         </div>
         <div className="phoneNumberWrapper wrapper">
           <div className="phoneNumber text">휴대전화 번호</div>
@@ -121,8 +133,12 @@ const Signup = () => {
             type="text"
             placeholder="휴대전화 번호를 입력해주세요"
             name="phone_number"
+            onChange={saveUserInfo}
+            onKeyUp={checkPhoneNumber}
           />
-          <div className="message" />
+          <div className="message">
+            <span className={redWarningPhoneNumber}>{phoneNumberWarning}</span>
+          </div>
         </div>
         <div className="passwordWrapper wrapper">
           <div className="password text">비밀번호</div>
@@ -130,24 +146,31 @@ const Signup = () => {
             type="password"
             placeholder="비밀번호를 입력해주세요"
             name="password"
+            onChange={saveUserInfo}
+            onKeyUp={checkPassword}
           />
-          <div className="message" />
+          <div className="message">
+            <span className={redWarningPassword}>{passwordWarning}</span>
+          </div>
         </div>
         <div className="confirmPasswordWrapper wrapper">
           <div className="confirmPassword text">비밀번호 확인</div>
-          <input
-            type="password"
-            placeholder="비밀번호를 다시 입력해주세요"
-            name="confirmPassword"
-          />
+          <input type="password" placeholder="비밀번호를 다시 입력해주세요" />
           <div className="message" />
         </div>
-        <label className="marketingAgree wrapper">
-          <input type="checkbox" name="event_push" />
-          이벤트 및 할인 소식 알림 동의 (선택)
-        </label>
+        {/* <label className="marketingAgree wrapper">
+          <input type="checkbox" name="event_push" className="checkbox" />
+          <span className="optionalMessage">
+            이벤트 및 할인 소식 알림 동의 (선택)
+          </span>
+        </label> */}
         <div className="signupButton wrapper">
-          <button type="button" className="button">
+          <button
+            type="button"
+            className="button"
+            onClick={postUserData}
+            disabled={isAllTrue ? false : true}
+          >
             동의하고 회원가입
           </button>
         </div>
