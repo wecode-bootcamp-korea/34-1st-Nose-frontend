bfc810338b884aea0e0cbe4e9650efd4b0f7d51c
정현준 <slosh@jeonghyeonjun-ui-MacBookAir.local>
2022-06-22 10:04:23 +0900

    [ADD] main 브런치 생성

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,7 +2,52 @@ import React from 'react';
 import './Main.scss';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div className="mainContainer">
+        <div className="textContent">
+          <div className="prefixTitle">1234</div>
+          <div className="suffixTitle">5678</div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="textContent">
+          <div className="prefixTitle">1234</div>
+          <div className="suffixTitle">5678</div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="textContent">
+          <div className="prefixTitle">1234</div>
+          <div className="suffixTitle">5678</div>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <div className="prefixTitle">1234</div>
+          <div className="suffixTitle">5678</div>
+        </div>
+      </div>
+      <div className="container">
+        <div>
+          <div className="prefixTitle">1234</div>
+          <div className="suffixTitle">5678</div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="textContent">
+          <div className="prefixTitle">1234</div>
+          <div className="suffixTitle">5678</div>
+        </div>
+      </div>
+      <div className="lastContainer">
+        <div>
+          <div className="prefixTitle">1234</div>
+          <div className="suffixTitle">5678</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
