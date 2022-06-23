5a42aeda8a2281789adc98fa9e58c7dd566c6314
Vanessa <bohyunparkv@gmail.com>
2022-06-23 21:18:54 +0900

    ADD: Shop UI

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Shop.scss';
 
 const Shop = () => {
-  return <div>Shop</div>;
+  const [perfumes, setPerfumes] = useState([]);
+
+  useEffect(() => {
+    fetch('..');
+  });
+
+  return <div className="">Shop</div>;
 };
 
 export default Shop;
