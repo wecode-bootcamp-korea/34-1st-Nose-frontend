fd10e10c0645e8a9ee7ab08cb22ff211c83fa4f3
Vanessa <bohyunparkv@gmail.com>
2022-06-23 20:12:40 +0900

    REFACTOR: 항목 수정

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Shop.scss';
 
 const Shop = () => {
-  const [perfumes, setPerfumes] = useState({});
   return <div>Shop</div>;
 };
 
