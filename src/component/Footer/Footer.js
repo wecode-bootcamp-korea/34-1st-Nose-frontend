fa912402806cd7be919a4b9237fd97272d3673ee
정현준 <slosh@jeonghyeonjun-ui-MacBookAir.local>
2022-06-23 21:08:19 +0900

    [REFACTOR] review반영

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -21,49 +21,47 @@ const Footer = () => {
         </div>
         <div className="sections sectionsMargin">
           <div className="section">이메일|help@paffem.com</div>
-          <div className="section">
-            <div>공지사항</div>
-            <div>FAQ</div>
-            <div>1:1문의</div>
-          </div>
-          <div className="section">
-            <div>친구 추천 포인트</div>
-            <div>2.5ML 교환 서비스</div>
-          </div>
-          <div className="section">
-            <div>Find Your Signature</div>
-            <div>Eco friendly</div>
-            <div>Better but different</div>
-          </div>
+          {INFODATA.map(text => {
+            return (
+              <div className="section" key={text.id}>
+                <div className="sectionText">{text.textTop}</div>
+                <div className="sectionText">{text.textMiddle}</div>
+                <div className="sectionText">{text.textBottom}</div>
+              </div>
+            );
+          })}
         </div>
         <div className="informationContainer">
           <div className="imgBox">
             <a href="https://www.instagram.com/wecode_bootcamp/?hl=ko">
-              <img src="/images/footer/instagram.png" alt="instagramLogo" />
+              <img
+                className="logo"
+                src="/images/footer/instagram.png"
+                alt="instagramLogo"
+              />
             </a>
-            <img src="/images/footer/facebook.png" alt="facebookLogo" />
-            <img src="/images/footer/brunch.png" alt="brunchLogo" />
+            <img
+              className="logo"
+              src="/images/footer/facebook.png"
+              alt="facebookLogo"
+            />
+            <img
+              className="logo"
+              src="/images/footer/brunch.png"
+              alt="brunchLogo"
+            />
           </div>
           <div className="mention">
             <strong>Find Your Signature, PAFFEM</strong>
           </div>
           <div className="information">
-            <div className="text">주식회사 파펨</div>
-            <div className="text">
-              주소:서울 중구 청계천로 100 시그니처타워 서관
-              1103호|사업자등록번호:792-87-00153 <strong>사업자정보확인</strong>
-              |상호:파펨|대표:최영열
-            </div>
-            <div className="text">
-              고객 문의 이메일 : help@paffem.com| 고객 센터 :
-              070-4948-8028|통신판매업신고번호: 제2020-서울중구-2566호|
-              <strong>개인정보취급방침</strong>및<strong>이용약관</strong>
-              |개인정보관리책임자:최영열
-            </div>
-            <div className="text">
-              Create by <strong>BIGNO STUDIO</strong>. @ PAFFEM ALL right
-              reserved.
-            </div>
+            {FOOTERDATA.map(text => {
+              return (
+                <div className="text" key={text.id}>
+                  {text.text}
+                </div>
+              );
+            })}
           </div>
           <div className="footerPaffem">PAFFEM</div>
         </div>
@@ -72,4 +70,38 @@ const Footer = () => {
   );
 };
 
+const INFODATA = [
+  {
+    id: 1,
+    textTop: '공지사항',
+    textMiddle: 'FAQ',
+    textBottom: '1:1문의',
+  },
+  {
+    id: 2,
+    textTop: '친구 추천 포인트',
+    textMiddle: '2.5ML 교환 서비스',
+    textBottom: '',
+  },
+  {
+    id: 3,
+    textTop: 'Find Your Signature',
+    textMiddle: 'Eco friendly',
+    textBottom: 'Better but different',
+  },
+];
+const FOOTERDATA = [
+  {
+    id: 1,
+    text: '주식회사 파펨',
+  },
+  {
+    id: 2,
+    text: '주소:서울 중구 청계천로 100 시그니처타워 서관1103호|사업자등록번호:792-87-00153 사업자정보확인|상호:파펨|대표:최영열',
+  },
+  {
+    id: 3,
+    text: 'Create by BIGNO STUDIO. @ PAFFEM ALL right reserved.',
+  },
+];
 export default Footer;
