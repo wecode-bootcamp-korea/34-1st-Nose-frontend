f4ec0a424e86354f09a8b0bef3b95c746622822a
정현준 <slosh@jeonghyeonjun-ui-MacBookAir.local>
2022-06-24 14:22:11 +0900

    [REFACTOR] review 반영

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -5,6 +5,7 @@ import './Nav.scss';
 
 const Nav = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const updateScroll = () => {
     setScrollPosition(window.scrollY || document.documentElement.scrollTop);
@@ -14,7 +15,6 @@ const Nav = () => {
     window.addEventListener('scroll', updateScroll);
   });
 
-  const [modalOpen, setModalOpen] = useState(false);
   const openModal = () => {
     setModalOpen(true);
   };
@@ -22,63 +22,60 @@ const Nav = () => {
     setModalOpen(false);
   };
 
+  const scrollNavHandle = () => {
+    return scrollPosition < 5 ? '' : ' changeNav';
+  };
+
+  const scrollLinktagHandle = () => {
+    return scrollPosition < 5 ? '' : ' changeLinktagText';
+  };
+
+  const scrollimgHandle = () => {
+    return scrollPosition < 5 ? '' : ' changeImg';
+  };
+
   return (
-    <nav className={`nav${scrollPosition < 5 ? '' : ' changeNav'}`}>
+    <nav className={`nav${scrollNavHandle()}`}>
       <div className="navCategory">
-        <ul className="navText">
-          <Link
-            to="/Survey"
-            className={scrollPosition < 5 ? '' : 'changeAtagText'}
-          >
+        <div className="navText">
+          <Link to="/Survey" className={`linkText${scrollLinktagHandle()}`}>
             Perfume Teller
           </Link>
-        </ul>
-        <ul className="navText shop">
-          <div
-            className={scrollPosition < 5 ? '' : 'changeAtagText'}
-            onMouseOver={openModal}
-          >
+        </div>
+        <div className="navText shop">
+          <div className={scrollLinktagHandle()} onMouseOver={openModal}>
             SHOP
           </div>
-          <Modal
-            open={modalOpen}
-            close={() => {
-              closeModal();
-            }}
-          />
-        </ul>
-        <ul className="navText">
-          <div className={scrollPosition < 5 ? '' : 'changeAtagText'}>
-            ABOUT
-          </div>
-        </ul>
-        <ul className="navText">
-          <div className={scrollPosition < 5 ? '' : 'changeAtagText'}>
-            EVENT
-          </div>
-        </ul>
+          <Modal modalOpen={modalOpen} closeModal={closeModal} />
+        </div>
+        <div className="navText">
+          <div className={scrollLinktagHandle()}>ABOUT</div>
+        </div>
+        <div className="navText">
+          <div className={scrollLinktagHandle()}>EVENT</div>
+        </div>
       </div>
       <div className="paffem">
-        <Link to="/" className={scrollPosition < 5 ? '' : 'changeAtagText'}>
+        <Link to="/" className={`linkText${scrollLinktagHandle()}`}>
           PAFFEM
         </Link>
       </div>
       <div className="navUserIcon">
         <Link to="/Login">
           <img
-            className={scrollPosition < 5 ? '' : 'changeImg'}
+            className={`navIcon${scrollimgHandle()}`}
             alt="user-icon"
             src="/images/nav/nav-user.png"
           />
         </Link>
         <img
-          className={scrollPosition < 5 ? '' : 'changeImg'}
+          className={`navIcon${scrollimgHandle()}`}
           alt="logout-icon"
           src="/images/nav/nav-logout.png"
         />
         <Link to="/Cart">
           <img
-            className={scrollPosition < 5 ? '' : 'changeImg'}
+            className={`navIcon${scrollimgHandle()}`}
             alt="cart-icon"
             src="/images/nav/nav-cart.png"
           />
--- src/component/Nav/component/Modal.js
+++ src/component/Nav/component/Modal.js
@@ -2,26 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Modal.scss';
 
-const Modal = ({ open, close }) => {
+const Modal = ({ modalOpen, closeModal }) => {
   return (
-    <div>
-      <div className={open ? 'openModal modal' : 'modal'}>
-        {open ? (
-          <div className="modalContent" onMouseLeave={() => close()}>
-            <div className="modalTextContainer">
-              <Link to="/Shop">
-                <div className="modalText">2.5ml / 나만의 디스커버리 셋트</div>
-              </Link>
-              <div className="modalText">40ml</div>
-              <div className="modalText">Curation</div>
-              <div className="modalText">GOODS</div>
-            </div>
-            <div className="imgBox">
-              <img alt="aaa" src="/images/nav/modal-perffume.jpg" />
-            </div>
+    <div className={modalOpen ? 'openModal modal' : 'modal'}>
+      {modalOpen && (
+        <div className="modalContent" onMouseLeave={closeModal}>
+          <div className="modalTextContainer">
+            <Link className="linkText" to="/shop">
+              <div className="modalText">2.5ml / 나만의 디스커버리 셋트</div>
+            </Link>
+            <div className="modalText">40ml</div>
+            <div className="modalText">Curation</div>
+            <div className="modalText">GOODS</div>
           </div>
-        ) : null}
-      </div>
+          <div className="imgBox">
+            <img
+              className="modalImg"
+              alt="aaa"
+              src="/images/nav/modal-perffume.jpg"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
