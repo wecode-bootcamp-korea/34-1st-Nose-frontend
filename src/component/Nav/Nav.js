2be77155c891a32e7183a287aa82929c48b1c5f7
201steve <79638133+201steve@users.noreply.github.com>
2022-06-27 16:34:25 +0900

    nav 코드리뷰

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -21,61 +21,64 @@ const Nav = () => {
   };
 
   const isScrollMoved = scrollPosition < 5;
+  const toggleNavColor = isScrollMoved ? '' : ' changeNav';
+  const switchLinkColor = isScrollMoved ? '' : ' changeLinktagText';
+  const toggleIconColor = isScrollMoved ? '' : ' changeImg';
 
-  const scrollNavHandle = () => {
-    return isScrollMoved ? '' : ' changeNav';
-  };
+  // const scrollNavHandle = () => {
+  //   return isScrollMoved ? '' : ' changeNav';
+  // };
 
-  const scrollLinktagHandle = () => {
-    return isScrollMoved ? '' : ' changeLinktagText';
-  };
+  // const scrollLinktagHandle = () => {
+  //   return isScrollMoved ? '' : ' changeLinktagText';
+  // };
 
-  const scrollimgHandle = () => {
-    return isScrollMoved ? '' : ' changeImg';
-  };
+  // const scrollimgHandle = () => {
+  //   return isScrollMoved ? '' : ' changeImg';
+  // };
 
   return (
-    <nav className={`nav${scrollNavHandle()}`}>
+    <nav className={`nav${toggleNavColor}`}>
       <div className="navCategory">
         <div className="navText">
-          <Link to="/Survey" className={`linkText${scrollLinktagHandle()}`}>
+          <Link to="/Survey" className={`linkText${switchLinkColor}`}>
             Perfume Teller
           </Link>
         </div>
         <div className="navText shop">
-          <div className={scrollLinktagHandle()} onMouseOver={openModal}>
+          <div className={switchLinkColor} onMouseOver={openModal}>
             SHOP
           </div>
           <Modal modalOpen={modalOpen} closeModal={closeModal} />
         </div>
         <div className="navText">
-          <div className={scrollLinktagHandle()}>ABOUT</div>
+          <div className={switchLinkColor}>ABOUT</div>
         </div>
         <div className="navText">
-          <div className={scrollLinktagHandle()}>EVENT</div>
+          <div className={switchLinkColor}>EVENT</div>
         </div>
       </div>
       <div className="paffem">
-        <Link to="/" className={`linkText${scrollLinktagHandle()}`}>
+        <Link to="/" className={`linkText${switchLinkColor}`}>
           PAFFEM
         </Link>
       </div>
       <div className="navUserIcon">
         <Link to="/Login">
           <img
-            className={`navIcon${scrollimgHandle()}`}
+            className={`navIcon${toggleIconColor}`}
             alt="user-icon"
             src="/images/nav/nav-user.png"
           />
         </Link>
         <img
-          className={`navIcon${scrollimgHandle()}`}
+          className={`navIcon${toggleIconColor}`}
           alt="logout-icon"
           src="/images/nav/nav-logout.png"
         />
         <Link to="/Cart">
           <img
-            className={`navIcon${scrollimgHandle()}`}
+            className={`navIcon${toggleIconColor}`}
             alt="cart-icon"
             src="/images/nav/nav-cart.png"
           />
